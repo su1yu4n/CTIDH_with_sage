c4cdfe9bafa8e0efe8426650fe3fd66f4e939e3c
su1yu4n <su1yu4n@gmail.com>
2024-01-04 15:25:52 +0800

    Change lambdas of PrimeField such as show_runtime into classmethod, and create mont.py
    Signed-off-by: su1yu4n <su1yu4n@gmail.com>

--- CTIDH/primefield.py
+++ CTIDH/primefield.py
@@ -3,11 +3,6 @@ from sage.rings.finite_rings.integer_mod import IntegerMod_gmp, IntegerMod_int
 
 proof.arithmetic(False)
 
-def reset_runtime(field):
-	field.add_count = 0
-	field.sqr_count = 0
-	field.mul_count = 0
-    
 
 def PrimeField(p: int):
     if not is_prime(p):
@@ -24,8 +19,22 @@ def PrimeField(p: int):
         else:
             raise TypeError('Cannot get the value of (type:{}) {}!'.format(type(other), other))
 
+    # TODO: Maybe rewrite get_value and operators with this decorator. Just for touching fish and learning python ×
+    # def check_other_type_and_return(f):
+    #   def wrapper(x, other):
+    #       if isinstance(other, ZModPrime):
+    #           return f(x.x, other.x)
+    #       elif isinstance(other, int):
+    #           return f(x.x, other)
+    #   return wrapper
 
-    class ZModPrime:            
+    class ZModPrime:        
+        add_count = 0
+        sqr_count = 0
+        mul_count = 0
+        pow_count = 0
+        inv_count = 0    
+
         # self.x always has the type IntegerMod_gmp when p is large or IntegerMod_int
         def __init__(self, x):
             if isinstance(x, IntegerMod_gmp) or isinstance(x, IntegerMod_int):
@@ -77,7 +86,7 @@ def PrimeField(p: int):
             self.x = self.x * other
             return self
         
-        # TODO: Maybe add div rdiv and idiv?
+        # TODO: Maybe implment div rdiv and idiv?
         def __div__(self, other):
             raise NotImplementedError
         
@@ -108,27 +117,27 @@ def PrimeField(p: int):
             ret.x = self.x
             return ret
         
+        @classmethod
+        def reset_runtime(cls):
+            cls.add_count = 0
+            cls.sqr_count = 0
+            cls.mul_count = 0
 
-    ZModPrime.add_count = 0
-    ZModPrime.sqr_count = 0
-    ZModPrime.mul_count = 0
-
-    ZModPrime.pow_count = 0
-    ZModPrime.inv_count = 0
-
-    ZModPrime.show_runtime = lambda label='Since last init_runtime': print(
-        "| %s: %7dM + %7dS + %7da"
-        % (label, ZModPrime.mul_count, ZModPrime.sqr_count, ZModPrime.add_count),
-        end="\t",
-    )
-
-    ZModPrime.reset_runtime = lambda: reset_runtime(ZModPrime)
-
-    ZModPrime.show_sqr_pow = lambda label='The whole process': print(
-        "| %s: %2dP + %2dI"
-        % (label, ZModPrime.pow_count, ZModPrime.inv_count),
-        end="\t",
-    )
+        @classmethod
+        def show_runtime(cls, label:str):
+            print(
+                "| %s: %7dM + %7dS + %7da"
+                % (label, cls.mul_count, cls.sqr_count, cls.add_count),
+                end="\t",
+            )
+        @classmethod
+        def show_sqr_pow(cls, label:str):
+            print(
+                "| %s: %2dP + %2dI"
+                % (label, cls.pow_count, cls.inv_count),
+                end="\t",
+            )
+            
 
     return ZModPrime
 
