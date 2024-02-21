e3d06e625d710609bdcbe9716447a09f86a68707
Yifan Zheng <su1yu4n@gmail.com>
2024-02-21 20:49:44 +0800

    Init isogeny algorithms test files
    Signed-off-by: Yifan Zheng <su1yu4n@gmail.com>

---
+++ tests/test_isogeny.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestMontgomeryIsogeny(unittest.TestCase):
+    def test_kps_t(self):
+        pass
+    def test_xisog_t(self):
+        pass
+    def test_xeval_t(self):
+        pass
+    def test_kps_s(self):
+        pass
+    def test_xisog_s(self):
+        pass
+    def test_xeval_s(self):
+        pass
