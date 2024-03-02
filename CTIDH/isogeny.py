79cfe3c63600c7fd842c803f34ada3f92c769876
Yifan Zheng <su1yu4n@gmail.com>
2024-03-02 10:02:03 +0800

    Feat: Modify xeval_t to make it timing-safe.
    Signed-off-by: Yifan Zheng <su1yu4n@gmail.com>

--- CTIDH/isogeny.py
+++ CTIDH/isogeny.py
@@ -259,11 +259,12 @@ def MontgomeryIsogeny(formula_name='tvelu', uninitialized = False):
             X_hat, Z_hat = X+Z, X-Z
             X1_hat, Z1_hat = Xi_Zi_hats[0]
             X_prime, Z_prime = self.curve.crisscross(X1_hat, Z1_hat, X_hat, Z_hat)
-            # TODO: Change to constant-time
-            for i in range(1, d):
+            for i in range(1, d_fake):
                 Xi_hat, Zi_hat = Xi_Zi_hats[i]
                 t0, t1 = self.curve.crisscross(Xi_hat, Zi_hat, X_hat, Z_hat)
-                X_prime, Z_prime = t0*X_prime, t1*Z_prime
+                tmp1, tmp2 = t0*X_prime, t1*Z_prime
+                X_prime = CMOV(X_prime, tmp1, i<=d-1)
+                Z_prime = CMOV(Z_prime, tmp2, i<=d-1)
             X_prime, Z_prime = X*X_prime**2, Z*Z_prime**2
             return X_prime, Z_prime
 
